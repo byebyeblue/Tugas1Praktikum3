5c0e0f1a1e97d6311466fb38b1c3ff40a49135f3
byebyeblue <engtalker.id@gmail.com>
2021-09-16 21:24:45 +0700

    first commit

---
+++ app/Views/v_about.php
@@ -0,0 +1,7 @@
+<div class="container mt-3">
+    <div class="text-center">
+        <img src="https://www.nicepng.com/png/full/201-2015346_linux-logo-png-linux-logo-transparent-png.png" alt="logo" width="180" class="img-thumbnail rounded-circle">
+        <h1 class="display-4">Praktikum Web Lanjut Dengan Studi Kasus Blog App</h1>
+        <p class="lead">Programming | Codeigniter 4 | Php</p>
+    </div>
+</div>
